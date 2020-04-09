83f967aca6d7bc04c0aed7c69a35d6c60c4dd497
Pavel Farkas <pavel.farkas@ezuce.com>
2020-04-10 01:43:37 +0200

    Update of host setup script, curl instead of git clone

--- reach-host-setup.sh
+++ reach-host-setup.sh
@@ -9,7 +9,7 @@ DEFAULT="$(tput sgr0)"
 if which yum > /dev/null 2>&1; then
 {
     yum -y update
-    yum -y install yum-utils device-mapper-persistent-data lvm2 git ngrep tcpdump net-tools sudo
+    yum -y install yum-utils device-mapper-persistent-data lvm2 ngrep tcpdump net-tools sudo
     yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
     yum -y install docker-ce
     systemctl start docker
@@ -19,7 +19,7 @@ elif which apt-get > /dev/null 2>&1; then
 {
     apt-get remove docker docker-engine docker.io
     apt-get -y update
-    apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common git ngrep tcpdump net-tools sudo
+    apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common ngrep tcpdump net-tools sudo
     curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
     add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
     apt-get -y update
@@ -36,17 +36,17 @@ chmod +x /usr/local/bin/docker-compose
 # create user ezuce, if it does not exist
 id -u ezuce >/dev/null 2>&1 || useradd --home-dir /home/ezuce -g docker ezuce
 
-#mkdir -p /home/ezuce/keys
-#mkdir -p /home/ezuce/keys-challenge/.well-known/acme-challenge
-
 # get installation scripts
 cd /home/ezuce
-git clone -b master --depth 1 https://github.com/ezuce/reach-install.git 2> /dev/null
+mkdir -p reach-install
+curl https://raw.githubusercontent.com/ezuce/reach-install/master/reach > reach-install/reach
+curl https://raw.githubusercontent.com/ezuce/reach-install/master/docker-compose.yml > reach-install/docker-compose.yml
+chmod +x reach-install/reach
 chown ezuce:ezuce -R /home/ezuce
 
 # user ezuce can quietly alter iptables to open port range for RTP media
 echo "%ezuce ALL=(root) NOPASSWD:/sbin/iptables" > /etc/sudoers.d/ezuce
 
 cat <<-EOF
-	${FG_BOLD}Login now as user ${FG_GREEN_BOLD}ezuce${FG_BOLD} to continue ...${DEFAULT}
+	${FG_BOLD}To continue, login as user ${FG_GREEN_BOLD}ezuce${DEFAULT}${FG_BOLD} [ su - ezuce ] ...${DEFAULT}
 EOF
